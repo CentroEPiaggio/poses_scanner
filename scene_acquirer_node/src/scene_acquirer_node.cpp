6c19c8869c1e9cb646aa00a7630c22f2e9460bdf
Federico Spinelli <fspinelli@gmail.com>
2015-02-08 17:44:41 +0100

    Added passthrough filter to scene acquirer and parameters to handle it

--- scene_acquirer_node/src/scene_acquirer_node.cpp
+++ scene_acquirer_node/src/scene_acquirer_node.cpp
@@ -31,13 +31,20 @@ class sceneAcquirer
     //Message Subscriber
     ros::Subscriber sub_stream_;
     
+    //Message Publisher
+    ros::Publisher pub_stream_;
+    
     //Service callback, gets executed when service is called
     bool acquireScene(scene_acquirer_node::acquire_scene::Request& req, scene_acquirer_node::acquire_scene::Response& res);
 
     //Message callback, gets executed when a new message is available on topic
     void new_cloud_in_stream(const sensor_msgs::PointCloud2::ConstPtr& message);
 
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_stream_;
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr scene_stream_;
+
+    //parameters
+    bool filter_;
+    double xmin,xmax,ymin,ymax,zmin,zmax;
 };
 
 //Constructor
@@ -45,7 +52,7 @@ sceneAcquirer::sceneAcquirer()
 {
   nh_ = ros::NodeHandle("scene_acquirer_node");
   pcl::PointCloud<pcl::PointXYZRGBA> a;
-  cloud_stream_ = a.makeShared();
+  scene_stream_ = a.makeShared();
 
   //service callbacks
   srv_acquire_ = nh_.advertiseService("acquire_scene", &sceneAcquirer::acquireScene, this);
@@ -53,36 +60,77 @@ sceneAcquirer::sceneAcquirer()
   //subscribe to depth_registered pointclouds topic
   std::string topic = nh_.resolveName("/camera/depth_registered/points");
   sub_stream_ = nh_.subscribe(topic, 1, &sceneAcquirer::new_cloud_in_stream, this);
+
+  pub_stream_ = nh_.advertise<sensor_msgs::PointCloud2> ("/scene_acquirer/scene",1);
+
+  //load parameters
+  nh_.param<bool>("filter", filter_, "false");
+  nh_.param<double>("xmin", xmin, -100);
+  nh_.param<double>("xmax", xmax, 100);
+  nh_.param<double>("ymin", ymin, -100);
+  nh_.param<double>("ymax", ymax, 100);
+  nh_.param<double>("zmin", zmin, -100);
+  nh_.param<double>("zmax", zmax, 100);
 }
 
 void sceneAcquirer::new_cloud_in_stream(const sensor_msgs::PointCloud2::ConstPtr& message)
 {
-  //constantly copy cloud from stream into class cloud_stream_ to be accessible for service callback
-  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr stream (new pcl::PointCloud<pcl::PointXYZRGBA> );
-  pcl::PassThrough<pcl::PointXYZRGBA> pass;
-  pcl::fromROSMsg (*message, *stream);
-  pass.setInputCloud (stream);
-  pass.setFilterFieldName ("z");
-  pass.setFilterLimits (0.7, 1.8);
-  pass.filter (*cloud_stream_);
-  pass.setInputCloud (cloud_stream_);
-  pass.setFilterFieldName ("y");
-  pass.setFilterLimits (-2, 0.3);
-  pass.filter (*cloud_stream_);
+  //constantly copy cloud from stream into class scene_stream_ to be accessible for service callback
+  pcl::fromROSMsg (*message, *scene_stream_);
+
+  //check if we need to filter stream
+  nh_.getParam("filter", filter_);
+  if (filter_)
+  {
+    nh_.getParam("xmin", xmin);
+    nh_.getParam("xmax", xmax);
+    nh_.getParam("ymin", ymin);
+    nh_.getParam("zmin", zmin);
+    nh_.getParam("ymax", ymax);
+    nh_.getParam("zmax", zmax);
+    pcl::PassThrough<pcl::PointXYZRGBA> pass;
+    pass.setInputCloud (scene_stream_);
+    
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (zmin, zmax);
+    pass.filter (*scene_stream_);
+    
+    pass.setInputCloud (scene_stream_);
+    pass.setFilterFieldName ("y");
+    pass.setFilterLimits (ymin, ymax);
+    pass.filter (*scene_stream_);
+    
+    pass.setInputCloud (scene_stream_);
+    pass.setFilterFieldName ("x");
+    pass.setFilterLimits (xmin, xmax);
+    pass.filter (*scene_stream_);
+
+    sensor_msgs::PointCloud2 msg;
+    pcl::toROSMsg (*scene_stream_, msg);
+    pub_stream_.publish( msg ); //republish the modified scene
+  }
+  else
+  {
+    pub_stream_.publish( *message ); //or republish the same scene
+  }
+  
 }
 
 bool sceneAcquirer::acquireScene (scene_acquirer_node::acquire_scene::Request& req, scene_acquirer_node::acquire_scene::Response& res)
 {
   if (req.save.compare("false") != 0)
   {
+    //user requested the scene to be saved on disk, lets comply him
     std::string home = std::getenv( "HOME" );
     pcl::PCDWriter writer;
-    writer.writeBinaryCompressed ((home + "/" + req.save + ".pcd").c_str(), *cloud_stream_);
+    writer.writeBinaryCompressed ((home + "/" + req.save + ".pcd").c_str(), *scene_stream_);
+    ROS_INFO("[sceneAcquirer] Scene saved to %s", (home + "/" + req.save + ".pcd").c_str() );
   }
+  //also send it to service response
   sensor_msgs::PointCloud2 msg;
-  pcl::toROSMsg(*cloud_stream_, msg);
+  pcl::toROSMsg(*scene_stream_, msg);
   res.cloud = msg; 
-  ROS_INFO("[sceneAcquirer] Sent cloud to service response");
+  ROS_INFO("[sceneAcquirer] Sent scene to service response");
   return true;
 }
 
@@ -90,8 +138,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "scene_acquirer_node");
     sceneAcquirer scene_acquirer_node;
-    ROS_INFO("[sceneAcquirer] Syncing to /camera/depth_registered/points...");
-    ROS_INFO("[sceneAcquirer] Waiting service call to grab a scene...");
-    ros::spin();
+    ROS_INFO("[sceneAcquirer] Node is ready");
+    ros::spin(); //go as fast as you can !
     return 0;
 }
