f05e7112f847aa1430b2f6a0c7c1d24204d480e2
Federico Spinelli <fspinelli@gmail.com>
2015-09-28 16:50:32 +0200

    fixed compilation

--- src/table_interface_node.cpp
+++ src/table_interface_node.cpp
@@ -16,23 +16,23 @@
 #include <string.h>
 
 #include "qb_cube_lib.h"
-#include "turn_table_interface_node/setPos.h"
-#include "turn_table_interface_node/getPos.h"
+#include "turn_table_interface/setPos.h"
+#include "turn_table_interface/getPos.h"
 
-class turnTable
+class TurnTable
 {
 public:
-  turnTable();
-  virtual ~turnTable();
+  TurnTable();
+  virtual ~TurnTable();
 
 private:
   ros::NodeHandle nh_;
   ros::ServiceServer srv_table_pos_, srv_read_pos_;
   //service callback
-  bool setTablePos(turn_table_interface_node::setPos::Request  &req,
-                        turn_table_interface_node::setPos::Response &res );
-  bool getTablePos(turn_table_interface_node::getPos::Request  &req,
-                        turn_table_interface_node::getPos::Response &res );
+  bool setTablePos(turn_table_interface::setPos::Request  &req,
+                        turn_table_interface::setPos::Response &res );
+  bool getTablePos(turn_table_interface::getPos::Request  &req,
+                        turn_table_interface::getPos::Response &res );
 
   std::string port_;
   double encoderRate_;
@@ -42,56 +42,56 @@ private:
   void connectToCube();
 };
 
-turnTable::turnTable()
+TurnTable::TurnTable()
 {
-  ROS_INFO("[turnTable] Starting turn table interface node");
-  nh_ = ros::NodeHandle("turn_table_interface_node");
+  ROS_INFO("[TurnTable] Starting turn table interface node");
+  nh_ = ros::NodeHandle("turn_table_interface");
   nh_.param<std::string>("port", port_,"/dev/ttyUSB0");
   nh_.param<double>("encoderRate", encoderRate_, DEG_TICK_MULTIPLIER);
-  
-  ROS_INFO_STREAM("[turnTable] Connecting to table at " << port_ );
-  
+
+  ROS_INFO_STREAM("[TurnTable] Connecting to table at " << port_ );
+
   cube_mutex_.lock();
   this->connectToCube();
   commActivate(&cube_comm_, 1, true); //cube_id is 1 for table
   cube_mutex_.unlock();
-  
-  srv_table_pos_ = nh_.advertiseService("set_table_pos", &turnTable::setTablePos, this);
-  srv_read_pos_ = nh_.advertiseService("get_table_pos", &turnTable::getTablePos, this);
-  
+
+  srv_table_pos_ = nh_.advertiseService("set_table_pos", &TurnTable::setTablePos, this);
+  srv_read_pos_ = nh_.advertiseService("get_table_pos", &TurnTable::getTablePos, this);
+
 }
 
-turnTable::~turnTable()
+TurnTable::~TurnTable()
 {
   cube_mutex_.lock();
   closeRS485(&cube_comm_);
   cube_mutex_.unlock();
-  ROS_INFO_STREAM("[turnTable] Communication closed");
+  ROS_INFO_STREAM("[TurnTable] Communication closed");
 }
 
-void turnTable::connectToCube() 
+void TurnTable::connectToCube()
 {
   openRS485(&cube_comm_, port_.c_str());
-  if(cube_comm_.file_handle <= 0) 
+  if(cube_comm_.file_handle <= 0)
   {
-    ROS_ERROR("[turnTable] Panic, cube file handle was invalid");
+    ROS_ERROR("[TurnTable] Panic, cube file handle was invalid");
     return;
   }
   else
   {
-    ROS_INFO_STREAM("[turnTable] Opened communication with file handle " << cube_comm_.file_handle);
+    ROS_INFO_STREAM("[TurnTable] Opened communication with file handle " << cube_comm_.file_handle);
   }
 }
 
-bool turnTable::setTablePos(turn_table_interface_node::setPos::Request  &req,
-             turn_table_interface_node::setPos::Response &res )
+bool TurnTable::setTablePos(turn_table_interface::setPos::Request  &req,
+             turn_table_interface::setPos::Response &res )
 {
   double position = req.position; // pos in degrees
 
-  ROS_INFO_STREAM("[turnTable] Sending Turn Table to position: " << position);
+  ROS_INFO_STREAM("[TurnTable] Sending Turn Table to position: " << position);
 
   short int inputs;
-  inputs = encoderRate_*position; 
+  inputs = encoderRate_*position;
   short int curr_ref[NUM_OF_MOTORS];
   curr_ref[0] = inputs;
   curr_ref[1] = inputs;
@@ -102,8 +102,8 @@ bool turnTable::setTablePos(turn_table_interface_node::setPos::Request  &req,
   return true;
 }
 
-bool turnTable::getTablePos(turn_table_interface_node::getPos::Request  &req,
-             turn_table_interface_node::getPos::Response &res )
+bool TurnTable::getTablePos(turn_table_interface::getPos::Request  &req,
+             turn_table_interface::getPos::Response &res )
 {
   short int measurements[3];
   double position;
@@ -111,15 +111,15 @@ bool turnTable::getTablePos(turn_table_interface_node::getPos::Request  &req,
   commGetMeasurements(&cube_comm_, 1, measurements);
   cube_mutex_.unlock();
   position = (double)(measurements[0]) /encoderRate_;
-  ROS_INFO_STREAM("[turnTable] Table position reads: " << position );
+  ROS_INFO_STREAM("[TurnTable] Table position reads: " << position );
   res.current_pos = (short int)position;
   return true;
 }
 
 int main( int argc, char* argv[] )
 {
-    ros::init(argc, argv, "turn_table_interface_node");
-    turnTable turn_table_node;
+    ros::init(argc, argv, "turn_table_interface");
+    TurnTable turn_table_node;
     ros::spin();
     return 0;
 }
